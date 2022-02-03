f20054ac34b68b66c2536dde50714f522bda82fa
leomunizq <leonardo.mdq@hotmail.com>
2022-02-03 22:49:01 +0100

    Home page.

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import illustrationHome from '../assets/images/Saly-39.svg'
+
+export function Home(){
+  return (
+    <main>
+    <div>
+    <strong>.youtime</strong>
+<img src={illustrationHome} alt="Illustration Robot" />
+
+<h2>Make yourself more on time</h2>
+<button>
+  ENTER
+</button>
+</div>
+</main>
+  )
+}
