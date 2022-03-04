0e6e88f84cdfcdc8a181650f276698cb71c8b418
leomunizq <leonardo.mdq@hotmail.com>
2022-03-04 10:53:36 +0100

    update Project name and all todos inside to Project.

--- yourtime/src/components/RenameProject.tsx
+++ yourtime/src/components/RenameProject.tsx
@@ -1,11 +1,59 @@
 import React, {useState} from 'react'
 import ProjectForm from './ProjectForm'
 
+import firebase from '../services/firebase'
+import 'firebase/compat/firestore'
+
 function RenameProject({project, setShowModal}: any){
     const [newProjectName, setNewProjectName] = useState(project.name)
 
-    function handleSubmit(e:any){
 
+
+    const renameProject = (project:any, newProjectName:any)=> {
+        const projectsRef = firebase.firestore().collection('projects')
+        const todosRef = firebase.firestore().collection('todos')
+        const {name: oldProjectName} = project
+
+        projectsRef
+        .where('name', '==', newProjectName)
+        .get()
+        .then( querySnapshot => {
+            if(!querySnapshot.empty){
+                alert('Project with the same name already exists!')
+
+            }else{
+                projectsRef
+                .doc(project.id)
+                .update({
+                    name : newProjectName
+                })
+                .then(() => {
+                    todosRef
+                    .where('project', '==', oldProjectName)
+                    .get()
+                    .then(querySnapshot => {
+                        querySnapshot.forEach(doc => {
+                            doc.ref.update({
+                                project: newProjectName
+                            })
+                        })
+                    })
+                })
+            }
+        })
+
+
+    }
+
+
+
+
+    function handleSubmit(e:any){
+        e.preventDefault()
+
+        renameProject(project, newProjectName)
+
+        setShowModal(false)
     }
 
     return (
